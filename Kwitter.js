ac7c487b2f85445596843a39fc02027915d95363
Yoshi-cyber <83883634+Yoshi-cyber@users.noreply.github.com>
2022-08-02 11:06:47 -0500

    Update Kwitter.js

--- Kwitter.js
+++ Kwitter.js
@@ -1,5 +1,7 @@
-function adduser() 
-{ 
-user_name = document.getElementById("user_name").value; l
-ocalStorage.setItem("user_name", user_name); window.location = "kwitter_room.html";
- }
+function adduser() {
+    user_name = document.getElementById("user_name").value;
+
+    localStorage.setItem("Username",user_name);
+
+    window.location = "kwitter_room.html";
+}
