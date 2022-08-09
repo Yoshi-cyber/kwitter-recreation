951ea7daea8aa4f46da387a7272d2985e87bf2e5
Yoshi-cyber <83883634+Yoshi-cyber@users.noreply.github.com>
2022-08-09 11:36:55 -0500

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -29,6 +29,7 @@ var firebaseConfig = {
  {
        firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
         Room_names = childKey;
+        
         console.log("Room Name - " + Room_names); 
         row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>"; 
         document.getElementById("output").innerHTML += row;
@@ -44,3 +45,20 @@ var firebaseConfig = {
        window.location = "kwitter_page.html"; 
  }
  
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+            window.location = "kwitter_room.html";
+      }
+      
+function send()
+      {
+            msg = document.getElementById("msg").value;
+            firebase.database().ref(room_name).push({
+                  name:user_name,
+                  message:msg,
+                  like:0
+            });
+      
+            document.getElementById("msg").value = "";
+      }
